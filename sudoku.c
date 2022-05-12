6bf981e7438fb9ee41d2f8cc90c2744d9efbff95
ALEJANDROMARI10 <>
2022-05-12 15:48:20 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,7 +45,7 @@ void print_node(Node* n){
 
 int is_valid(Node* n){
     int i,j;
-    int A[10];//,B[10],C[10];
+    int A[10],B[10],C[10];
     for(j=0;j<9;j++){
       for(i=0;i<9;i++){
         if(A[n->sudo[i][j]]==0){
